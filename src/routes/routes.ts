cecccf0132fe92e11bdee100071c5c8f76c3145a
Pablo <paul.campbell@tier1fin.com>
2022-05-25 18:37:49 -0500

    fix require cylce from route.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,31 +1,36 @@
-import * as Screens from '../components/organisms'
+import { Help } from '../components/organisms/Help'
+import { AddSound } from '../components/organisms/AddSound'
+import { Home } from '../components/organisms/Home'
+import { Login } from '../components/organisms/Login'
+import { Settings } from '../components/organisms/Settings'
+import { SignUp } from '../components/organisms/SignUp'
 
 export const unathenticatedRoute = [
   {
     name: 'Login',
-    component: Screens.Login,
+    component: Login,
   },
   {
     name: 'SignUp',
-    component: Screens.SignUp,
+    component: SignUp,
   },
 ]
 
 export const authenticatedRoute = [
   {
     name: 'Home',
-    component: Screens.Home,
+    component: Home,
   },
   {
-    name: 'AddSound',
-    component: Screens.AddSound,
+    name: 'Add Sound',
+    component: AddSound,
   },
   {
     name: 'Help',
-    component: Screens.Help,
+    component: Help,
   },
   {
     name: 'Settings',
-    component: Screens.Settings,
+    component: Settings,
   },
 ]
