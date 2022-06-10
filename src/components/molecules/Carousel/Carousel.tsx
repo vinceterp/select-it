67a97c169a992dc8d893bd88f235d4ac77a904a5
Pablo <paul.campbell@tier1fin.com>
2022-06-10 12:41:08 -0500

    add ref to carousel object

--- src/components/molecules/Carousel/Carousel.tsx
+++ src/components/molecules/Carousel/Carousel.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef, useState, ReactNode } from 'react';
 import { Image, View } from 'react-native';
 import { COLORS, styles } from '../../../styles';
 import Carousel from 'react-native-snap-carousel';
 import { Label } from '../../atoms';
 
+const data = [
+  <Image
+    source={require('../../../assets/sg_logo.png')}
+    style={styles.app({}).carouselImage}
+  />,
+  <Image
+    source={require('../../../assets/select_it_logo.png')}
+    style={styles.app({}).mediumImage}
+  />,
+];
+
 export default function MyCarousel() {
+  const carouselRef= useRef<Carousel<any>>(null);
   const renderItem = ({ item, index }: { item: any; index: number }) => {
     return (
       <View style={styles.app({}).carouselItemContainer}>
@@ -28,23 +40,15 @@ export default function MyCarousel() {
         justifyContent: 'center',
       }}
     >
-      <View style={{ height: 122, marginTop: 40 }}>
+      <View style={{display: 'flex', padding: 0, height: '65%', marginTop: 20, alignItems: 'center', justifyContent: 'center', width: '100%' }}>
         <Carousel
-          data={[
-            <Image
-              source={require('../../../assets/sg_logo.png')}
-              style={styles.app({}).carouselImage}
-            />,
-            <Image
-              source={require('../../../assets/select_it_logo.png')}
-              style={styles.app({}).mediumImage}
-            />,
-          ]}
+          data={data}
           renderItem={(props) => renderItem(props)}
           sliderWidth={400}
           itemWidth={290}
           layout={'default'}
           enableSnap
+          ref={carouselRef}
         />
       </View>
     </View>
