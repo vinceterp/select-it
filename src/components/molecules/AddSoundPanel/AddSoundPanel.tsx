3d3eca0e85b78fccd80ff9b6d711c40d614b8ed9
Pablo <paul.campbell@tier1fin.com>
2022-06-18 17:35:45 -0500

    add file functionality

--- src/components/molecules/AddSoundPanel/AddSoundPanel.tsx
+++ src/components/molecules/AddSoundPanel/AddSoundPanel.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
+import { COLORS } from '../../../styles';
+import { Button, Icon } from '../../atoms';
+import * as DocumentPicker from 'expo-document-picker';
 
 export default function AddSoundPanel() {
-  return <View style={{ height: '90%', padding: '5%' }}></View>;
+  const selectFile = useCallback(async () => {
+    const file = await DocumentPicker.getDocumentAsync({ multiple: false });
+    console.log(file);
+  }, []);
+
+  return (
+    <View
+      style={{
+        height: '90%',
+        padding: '5%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Button
+        buttonTheme="primary"
+        icon={<Icon name="Plus" fill={COLORS.WHITE} />}
+        title="Choose file"
+        onPress={() => selectFile()}
+      />
+    </View>
+  );
 }
