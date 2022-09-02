8e202e4f0b09a5174e523f0fabfc4ba1878b1a21
Pablo <paul.campbell@tier1fin.com>
2022-09-01 21:20:13 -0500

    google login complete

--- src/contexts/authentication/index.tsx
+++ src/contexts/authentication/index.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import * as Google from 'expo-auth-session/providers/google';
 
 export const initialUserDataContext = {
   email: '',
@@ -16,12 +17,14 @@ export interface AuthenticationContextType {
   userData: UserDataType;
   setUserData: (value: UserDataType) => void;
   logout: () => void;
+  loginWithGoogle: () => void;
 }
 
 export const AuthenticationContext = createContext<AuthenticationContextType>({
   userData: initialUserDataContext,
   setUserData: (value: UserDataType) => {},
   logout: () => {},
+  loginWithGoogle: () => {},
 });
 
 export const AuthenticationProvider = ({
@@ -29,9 +32,37 @@ export const AuthenticationProvider = ({
 }: {
   children: ReactNode;
 }) => {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: "988184132423-3tgnbdm6177p0rntvujaopqokg31rh0c.apps.googleusercontent.com",
+    iosClientId: "988184132423-9loleqi81s7514ed1rc9jo75hppsm2la.apps.googleusercontent.com",
+    androidClientId: "988184132423-npda4mhnp20ag92qac9q22mfbqkpatgk.apps.googleusercontent.com",
+  });
+
   const [userData, setUserData] = useState<UserDataType>(
     initialUserDataContext
   );
+  
+
+  const loginWithGoogle =  async () => {
+    console.log(process.env.IOS_CLIENT_ID);
+    try {
+      promptAsync();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    const castResponse = response as any;
+    try{
+      if (castResponse.authentication.accessToken){
+        setUserData((prevUserData) => ({ ...prevUserData, token: castResponse.authentication.accessToken }));
+      }
+    } catch(e) {
+      console.log('cancelled');
+    }
+    
+  }, [response]);
 
   const logout = () => {
     setUserData({ ...userData, token: '' });
@@ -40,6 +71,7 @@ export const AuthenticationProvider = ({
     userData,
     setUserData,
     logout,
+    loginWithGoogle,
   };
   return (
     <AuthenticationContext.Provider value={contextValue}>
